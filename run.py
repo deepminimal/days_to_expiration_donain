9619c479bfbedffdf87896af84f0cb4edf089d0b
deepminimal <voffko91@gmail.com>
2019-10-23 10:24:56 +0300

    Update run.py

--- run.py
+++ run.py
@@ -9,14 +9,7 @@ now = datetime.now()
 domain = argv[1]
 
 w = whois.query(domain)
-
-
-if type(w.expiration_date) == list:
-    w.expiration_date = w.expiration_date[0]
-else:
-    w.expiration_date = w.expiration_date
-
+w.expiration_date = w.expiration_date
 domain_expiration_date = str(w.expiration_date.day) + '/' + str(w.expiration_date.month) + '/' + str(w.expiration_date.year)
-
 timedelta = w.expiration_date - now
 print (timedelta.days)
